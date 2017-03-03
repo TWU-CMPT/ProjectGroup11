3002785c6f3feefb05bbb741443aeea597a67971
Nik Nem <nn@Niks-Mac.local>
2017-03-02 22:15:04 -0800

    Initial Commit

---
+++ snakattack2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  snakattack2
+//
+//  Created by Nik Nem on 2017-03-02.
+//  Copyright © 2017 CMPT276-Group11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
